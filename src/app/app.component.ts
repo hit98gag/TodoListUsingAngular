378068121b4a61d43b6bd222a8628631b5a27da8
Hitesh Tiwari <72449162+hit98gag@users.noreply.github.com>
2023-04-05 22:53:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
 
+
+interface Task {
+  isSelected: any;
+  id: number;
+  name: string;
+  isCompleted: boolean;
+  isEditing: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todolist';
+  title = 'Todo List';
+  tasks: Task[] = [];
+  newTaskName = '';
+  orderedList = false;
+
+
+  addTask() {
+    if (this.newTaskName.trim() !== '') {
+      const newTask: Task = {
+        id: this.tasks.length + 1,
+        name: this.newTaskName,
+        isCompleted: false,
+        isEditing: false,
+        isSelected: false
+        
+      };
+      this.tasks.push(newTask);
+      this.newTaskName = '';
+    }
+  }
+
+  removeTask(taskId: number) {
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+  removeAllTasks() {
+    this.tasks = [];
+  }
+
+  editTask(task: Task) {
+    task.isEditing = true;
+  }
+
+  saveTask(task: Task) {
+    task.isEditing = false;
+  }
+
+  toggleTaskCompletion(task: Task) {
+    task.isCompleted = !task.isCompleted;
+  }
+  toggleList() {
+    this.orderedList = !this.orderedList;
+  }
+
+  removeSelectedTasks() {
+    this.tasks = this.tasks.filter(task => !task.isSelected);
+  }
+
+  markTaskAsCompleted(task: Task) {
+    task.isCompleted = true;
+  }
+  
 }
